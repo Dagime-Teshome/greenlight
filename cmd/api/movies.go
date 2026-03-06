e2facdca8f3fa792b9e8f8ac0cb3c75f09a913a4
Dagime Teshome <dagimeteshome1@gmail.com>
2026-03-06 12:50:28 +0300

    send json on movie request

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/Dagime-Teshome/greenlight/internal/data"
 )
 
 func (app *app) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -16,5 +19,17 @@ func (app *app) showMovieHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Printf("showing movie details of %d", id)
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Casablanca",
+		Runtime:   102,
+		Genres:    []string{"drama", "romance", "war"},
+		Version:   1,
+	}
+	err = app.writeJson(w, http.StatusOK, movie, nil)
+	if err != nil {
+		app.logger.Print(err)
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+	}
 }
