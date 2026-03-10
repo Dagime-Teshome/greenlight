7a7fd03e6b24ddc92b6edfb5581e1a5fc9e3454b
Dagime Teshome <dagimeteshome1@gmail.com>
2026-03-10 11:45:08 +0300

    add error helper code

---
+++ cmd/api/errors.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func (app *app) logError(r *http.Request, err error) {
+	app.logger.Print(err)
+}
+
+func (app *app) errorResponse(w http.ResponseWriter, r *http.Request, status int, message any) {
+	env := Envelope{"error": message}
+
+	err := app.writeJson(w, status, env, nil)
+	if err != nil {
+		app.logError(r, err)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+}
+
+func (app *app) serverErrorResponse(w http.ResponseWriter, r *http.Request, err error) {
+	app.logError(r, err)
+	message := "the server encountered a problem and could not process your request"
+	app.errorResponse(w, r, http.StatusInternalServerError, message)
+
+}
+
+func (app *app) notFoundResponse(w http.ResponseWriter, r *http.Request) {
+	message := "the resource requested couldn't be found"
+	app.errorResponse(w, r, http.StatusNotFound, message)
+}
+
+func (app *app) methodNotAllowed(w http.ResponseWriter, r *http.Request) {
+	message := fmt.Sprintf("%s method not allowed on this resource", r.Method)
+	app.errorResponse(w, r, http.StatusMethodNotAllowed, message)
+}
