91808734e44e083d3df6c8df4e398d7127e4554f
Dagime Teshome <dagimeteshome1@gmail.com>
2026-03-12 19:56:11 +0300

    add custom unmarshal logic to runtime type

--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -1,9 +1,13 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
+
+var ErrInvalidRuntimeFormat = errors.New("invalid runtime format")
 
 type Runtime int32
 
@@ -13,4 +17,24 @@ func (r Runtime) MarshalJSON() ([]byte, error) {
 	quotedJson := strconv.Quote(jsonValue)
 
 	return []byte(quotedJson), nil
+}
+
+func (r *Runtime) UnMarshalJSON(data []byte) error {
+
+	unquotedJSONValue, err := strconv.Unquote(string(data))
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	parts := strings.Split(unquotedJSONValue, " ")
+
+	if len(parts) != 2 || parts[1] != "mins" {
+		return ErrInvalidRuntimeFormat
+	}
+
+	i, err := strconv.ParseInt(parts[0], 10, 32)
+
+	*r = Runtime(i)
+
+	return nil
 }
