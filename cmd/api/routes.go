c10c800ec5c2889c81db558f5c52bd829745a261
Dagime Teshome <dagimeteshome1@gmail.com>
2026-03-19 21:06:47 +0300

    add new rotues to router

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -12,8 +12,11 @@ func (app *app) routes() *httprouter.Router {
 	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowed)
 
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
+	// movies end points
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
-
+	router.HandlerFunc(http.MethodPost, "/v1/movies/", app.createMovieHandler)
+	router.HandlerFunc(http.MethodPut, "/v1/movies/:id", app.updateMoveHandler)
+	router.HandlerFunc(http.MethodDelete, "/v1/movies/:id", app.deleteMovieHandler)
 	return router
 }
