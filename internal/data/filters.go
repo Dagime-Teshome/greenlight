2e1e9827a2af017c1924da18f94d0ef11ebef1dc
Dagime Teshome <dagimeteshome1@gmail.com>
2026-03-23 17:52:45 +0300

    remove unnecessary space from filter validation

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -10,11 +10,9 @@ type Filters struct {
 }
 
 func ValidateFilters(v *validator.Validator, f Filters) {
-	// Check that the page and page_size parameters contain sensible values.
 	v.Check(f.Page > 0, "page", "must be greater than zero")
 	v.Check(f.Page <= 10_000_000, "page", "must be a maximum of 10 million")
 	v.Check(f.PageSize > 0, "page_size", "must be greater than zero")
 	v.Check(f.PageSize <= 100, "page_size", "must be a maximum of 100")
-	// Check that the sort parameter matches a value in the safelist.
 	v.Check(validator.PermittedValues(f.Sort, f.SortSafelist...), "sort", "invalid sort value")
 }
