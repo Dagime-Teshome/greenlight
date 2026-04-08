ae00b9afc6bfe3077c7449b3b0f961bd049bf749
Dagime Teshome <dagimeteshome1@gmail.com>
2026-04-08 17:05:11 +0300

    add new user handlers

---
+++ cmd/api/users.go
@@ -0,0 +1,20 @@
+package main
+
+import "net/http"
+
+func (app *app) createUserHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+func (app *app) getUserHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *app) updateUserHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+func (app *app) listUsersHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+func (app *app) deleteUserHandler(w http.ResponseWriter, r *http.Request) {
+
+}
