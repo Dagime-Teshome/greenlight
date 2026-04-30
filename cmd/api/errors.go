bd81fbe7e8795e18a8d3deaab813775f114f6ca5
Dagime Teshome <dagimeteshome1@gmail.com>
2026-04-30 21:32:36 +0300

    add invalid toke error

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -57,3 +57,8 @@ func (app *app) editConflictResponse(w http.ResponseWriter, r *http.Request) {
 	message := "unable to update the record due to an edit conflict, please try again"
 	app.errorResponse(w, r, http.StatusConflict, message)
 }
+func (app *app) invalidAuthenticationTokenResponse(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("WWW-Authenticate", "Bearer")
+	message := "invalid or missing authentication token"
+	app.errorResponse(w, r, http.StatusUnauthorized, message)
+}
