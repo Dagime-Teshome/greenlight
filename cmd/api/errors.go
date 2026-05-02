4b1bce8ba3edf3a0b0d1af4351afd3d3c55052c3
Dagime Teshome <dagimeteshome1@gmail.com>
2026-05-02 18:27:56 +0300

    add not permitted error type

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -72,3 +72,8 @@ func (app *app) unauthorizedUserResponse(w http.ResponseWriter, r *http.Request)
 	message := "your user account must be activated to access this resource"
 	app.errorResponse(w, r, http.StatusUnauthorized, message)
 }
+
+func (app *app) notPermittedResponse(w http.ResponseWriter, r *http.Request) {
+	message := "your user account doesn't have the necessary permissions to access this resource"
+	app.errorResponse(w, r, http.StatusForbidden, message)
+}
